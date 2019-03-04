11caaf82a79d4cea94551a4531da546f12a3aed8
ginko-ezbook <ginkobab@gmail.com>
2019-03-04 23:54:25 +0100

    installer almost fixed

--- scripts/system-installer.sh
+++ scripts/system-installer.sh
@@ -29,6 +29,11 @@ rm $HOME/.config/fish/fish_variables
 git --git-dir=$HOME/.dots --work-tree=$HOME checkout
 git --git-dir=$HOME/.dots --work-tree=$HOME config --local status.showUntrackedFiles no
 
+### --------------- SHELL COLORSCHEME ---------------
+git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
+cd $HOME/.config/base16-shell/scripts
+rm base16-flat.sh
+sed "s/\$color13/\$color21/" base16-gruvbox-dark-medium.sh > base16-flat.sh
 ### --------------- SUCKLESS ---------------
 cd $HOME/packs/dwm-git/src/dwm && sudo make clean install
 cd $HOME/packs/dmenu-git/src/dmenu && sudo make clean install
@@ -39,3 +44,5 @@ cd $HOME/packs/slstayts/src/sl && sudo make clean install
 
 ### --------------- DAEMONS ---------------
 systemctl enable --now tlp
+
+echo "Remember to enter 'base16-flat' to change da shell"
