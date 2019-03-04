e3fb781aaf8b9754594d78690750f84135861867
ginko-ezbook <ginkobab@gmail.com>
2019-03-04 23:07:31 +0100

    installer fix

--- scripts/system-installer.sh
+++ scripts/system-installer.sh
@@ -1,31 +1,30 @@
-#!usr/bin/fish
+#!/usr/bin/fish
 
 ### --------------- MAIN PACKAGES ---------------
 cd $HOME
 wget https://raw.githubusercontent.com/ginkobab/dots/master/.pkglist
-sudo pacman -S .pkglist
+sudo pacman -S (cat .pkglist)
 
 mkdir packs && cd packs
 
-git clone https://aur.archlinux.org/mellowplayer.git
-cd mellowplayer && makepkg -si && cd ..
+# git clone https://aur.archlinux.org/mellowplayer.git
+# cd mellowplayer && makepkg -si && cd ..
 
 git clone https://github.com/ginkobab/jupyterlab-vim
 
 ### --------------- FONT ---------------
-cd usr/share/fonts
+cd /usr/share/fonts
 wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/AnonymousPro/complete/Anonymice%20Nerd%20Font%20Complete%20Mono.ttf
 
 fc-cache
 
 ### --------------- DOT FILES --------------- 
 cd $HOME
-set -l dot "git --git-dir=$HOME/.dots --work-tree=$HOME"
 echo ".dots" >> .gitignore
 
 git clone --bare https://github.com/ginkobab/dots $HOME/.dots
-$dot checkout
-$dot config --local status.showUntrackedFiles no
+git --git-dir=$HOME/.dots --work-tree=$HOME checkout
+git --git-dir=$HOME/.dots --work-tree=$HOME config --local status.showUntrackedFiles no
 
 ### --------------- SUCKLESS ---------------
 cd $HOME/packs/dwm-git/src/dwm && sudo make clean install
